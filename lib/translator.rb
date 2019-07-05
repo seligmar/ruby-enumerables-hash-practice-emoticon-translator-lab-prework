970103f360a7db2dd36a56d30ef1b35403ca4fc1
Mary Selig <seligmar@gmail.com>
2019-07-05 15:41:36 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,43 @@
-# require modules here
+# # require modules here
+require "pry"
 
-def load_library
-  # code goes here
+require "yaml"  
+hash = YAML.load_file('./lib/emoticons.yml')  
+
+def load_library(hash)
+    emoticons_hash = {"get_meaning"=> {}, "get_emoticon"=> {}}
+    japanese_emoticons = []
+    english_emoticons = []
+    meanings = []
+    act = YAML.load_file('./lib/emoticons.yml')
+     act.each do |x, y| #y[0] = american, y[1] = japanese
+     japanese_emoticons << y[1]
+      english_emoticons << y[0] 
+      meanings << x 
+    end 
+    emoticons_hash["get_meaning"] = japanese_emoticons.zip(meanings).to_h
+    emoticons_hash["get_emoticon"] = english_emoticons.zip(japanese_emoticons).to_h
+emoticons_hash
+end 
+
+
+def get_japanese_emoticon(hash, emoticon)
+    answer = ""
+    hashes = load_library(hash)
+  #  binding.pry
+  #  if hashes["get_emoticon"].keys.include?(emoticon) 
+       hashes["get_emoticon"].select do |k,v| 
+            if k == emoticon 
+            answer = v 
+      #binding.pry
+      #[hashes["get_emoticon"]][emoticon].values 
+    #binding.pry
+  end 
+  end 
+answer
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_english_meaning(hash, emoticon)
+  load_library(hash)
 end
 
-def get_english_meaning
-  # code goes here
-end
