94d5a243e78b99977ba836569842af51157034cc
Mary Selig <seligmar@gmail.com>
2019-07-05 15:42:55 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -27,7 +27,7 @@ def get_japanese_emoticon(hash, emoticon)
        hashes["get_emoticon"].select do |k,v| 
             if k == emoticon 
               answer = v 
- # end 
+  end 
 #else 
   end 
 #answer
